a114f429519718a900f23861063b6e4d2979cbef
Jon Heise <j.heise@gmail.com>
2016-08-22 23:50:47 -0700

    fixed package name

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup( name="datastream",
        version='0.1',
        py_modules=['datastream'],
-       requires=['zmq>=15.0.0'],
+       requires=['pyzmq>=15.0.0'],
        url="https://github.com/jheise/datastream",
        author="Jon Heise",
        author_email="j.heise@gmail.com",
